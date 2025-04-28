71362f92d5fb97fbca5565e7aa9aa96a9e15564a
Brahim TAHIRI <pro.tahiri.brahim@gmail.com>
2025-04-28 20:05:49 +0200

    Initialisation du projet Educationsoft avec Spring Boot (sous Java 24).

---
+++ src/test/java/fr/brahimtahiri/educationsoft/EducationsoftApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.brahimtahiri.educationsoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationsoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
