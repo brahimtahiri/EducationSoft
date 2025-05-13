6305a020efefe7f3328f11fab9b237c7b1c7b728
Brahim TAHIRI <pro.tahiri.brahim@gmail.com>
2025-05-13 21:31:59 +0200

    Commit initial : Mise en place du projet Spring Boot avec structure API REST et support Thymeleaf

---
+++ src/main/java/fr/brahimtahiri/educationsoft/EducationsoftApplication.java
@@ -0,0 +1,13 @@
+package fr.brahimtahiri.educationsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationsoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducationsoftApplication.class, args);
+	}
+
+}
